7760194ca318644ca68ebbf70f3d37e974f199f3
Camus <ayenadedg@gmail.com>
2017-02-07 19:22:36 +0100

    update

--- src/tontineBundle/Entity/FicheTravail.php
+++ src/tontineBundle/Entity/FicheTravail.php
@@ -38,6 +38,36 @@ class FicheTravail
      */
     private $dateLivraison;
 
+    /**
+     * @var \tontineBundle\Entity\Agent
+     *
+     * @ORM\ManyToOne(targetEntity="tontineBundle\Entity\Agent", cascade={"persist"})
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="createdBy_id", referencedColumnName="id", nullable=true)
+     * })
+     */
+    private $createdBy;
+    
+   /**
+     * @var \tontineBundle\Entity\Agent
+     *
+     * @ORM\ManyToOne(targetEntity="tontineBundle\Entity\Agent", cascade={"persist"})
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="updatedBy_id", referencedColumnName="id", nullable=true)
+     * })
+     */
+    private $updatedBy;
+
+    /**
+     * @var \tontineBundle\Entity\Commande
+     *
+     * @ORM\ManyToOne(targetEntity="tontineBundle\Entity\Commande", cascade={"persist"})
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="commande_id", referencedColumnName="id", nullable=true)
+     * })
+     */
+    private $commande;
+
 
     /**
      * Get id
@@ -95,5 +125,77 @@ class FicheTravail
     public function getDateLivraison()
     {
         return $this->dateLivraison;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param \tontineBundle\Entity\Agent $createdBy
+     *
+     * @return FicheTravail
+     */
+    public function setCreatedBy(\tontineBundle\Entity\Agent $createdBy = null)
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return \tontineBundle\Entity\Agent
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * Set updatedBy
+     *
+     * @param \tontineBundle\Entity\Agent $updatedBy
+     *
+     * @return FicheTravail
+     */
+    public function setUpdatedBy(\tontineBundle\Entity\Agent $updatedBy = null)
+    {
+        $this->updatedBy = $updatedBy;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedBy
+     *
+     * @return \tontineBundle\Entity\Agent
+     */
+    public function getUpdatedBy()
+    {
+        return $this->updatedBy;
+    }
+
+    /**
+     * Set commande
+     *
+     * @param \tontineBundle\Entity\Commande $commande
+     *
+     * @return FicheTravail
+     */
+    public function setCommande(\tontineBundle\Entity\Commande $commande = null)
+    {
+        $this->commande = $commande;
+
+        return $this;
+    }
+
+    /**
+     * Get commande
+     *
+     * @return \tontineBundle\Entity\Commande
+     */
+    public function getCommande()
+    {
+        return $this->commande;
     }
 }
